21f7c053cd3535fd6c7583406d2b7e1416364c01
John Ichina <168111924+johnbryanichina@users.noreply.github.com>
2024-07-09 19:58:53 -0500

    Add files via upload

---
+++ view/visualizar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Visualizar documentos</title>
+    <link rel="stylesheet" href="../styles/estilos.css">
+     
+</head>
+<body>
+    <main>
+   
+    <?php include("../model/visualizardoc.php"); ?>
+        
+    </main><br><br>
+    <footer class="footer">
+            <p>©CodeMinds 2024</p>
+    </footer>
+</body>
+</html>
